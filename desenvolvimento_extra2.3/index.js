c9ea78ec584640898df40cbe9ee3d8a7f8074d1e
Ashley <ashleyscarllet@gmail.com>
2024-04-21 18:50:46 -0300

    Desenvolvimento Extra 2.3 criado.

---
+++ desenvolvimento_extra2.3/index.js
@@ -0,0 +1,16 @@
+let numerosDaSorte = [37, 14, 26, 5, 94, 87];
+
+numerosDaSorte.forEach((elemento) => {
+  if(elemento > 50) { 
+    console.log(`${elemento} é maior que 50.`)
+  }
+  else if (elemento % 2 == 0){
+    if (elemento < 50){
+      console.log(`${elemento} é par e menor que 50.`)
+    }
+  }
+  else {
+    console.log(`${elemento} é menor que 50.`)
+  }
+});
+
