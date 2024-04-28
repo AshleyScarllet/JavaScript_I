dec1a701adb454a0e5af224900d3c407585b4f52
Ashley <ashleyscarllet@gmail.com>
2024-04-27 21:27:51 -0300

    atividade feita

---
+++ desenvolvimento_extra_5.3/script.js
@@ -0,0 +1,27 @@
+let titulo = document.createElement("h1");
+let conteudo = document.createTextNode("Compre Aqui");
+titulo.appendChild(conteudo);
+document.body.appendChild(titulo);
+
+let produto = document.createElement("h3");
+let nome = document.createTextNode("Notebook Dell Inspiron 15 3530");
+produto.appendChild(nome);
+document.body.appendChild(produto);
+
+let preco = document.createElement("p");
+let valor = document.createTextNode("R$ 2.600,00");
+preco.appendChild(valor);
+document.body.appendChild(preco);
+
+let descricao = document.createElement("p");
+let texto = `
+Cor: Carbon Black - Preto Carbono<br>
+Tela: Full HD de 15.6" (1920 x 1080), 120 Hz, WVA<br>
+Processador: 13ª geração Intel® Core™ i5-1334U (10-core, cache de 12MB, até 4.60GHz)<br>
+Placa de vídeo: Intel® Iris® Xe com memória gráfica compartilhada<br>
+Memória RAM: 16GB DDR4 (2x8GB) 2666MT/s; Expansível até 16GB (2 slots soDIMM)<br>
+Armazenamento: SSD de 512GB PCIe NVMe M.2
+`;
+let informacao = document.createTextNode(texto);
+descricao.innerHTML = texto;
+document.body.appendChild(descricao);
